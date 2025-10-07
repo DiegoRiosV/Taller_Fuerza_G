99023d2ea9aa807be0c68a50bf3402d1c511cbaf
Diego Rios <diegoarv08@hotmail.com>
2025-10-07 13:49:23 -0400

    feat(pages): agregar páginas CRUD completas para Vehicle
    Se implementan las páginas Razor de Vehicle incluyendo:
    - Index, Create, Edit y Delete
    - Dropdowns de Owner y Model en Create/Edit
    - Uso de VehicleRepository, OwnerRepository y ModelRepository con inyección de dependencias
    - Variables claras en Razor para evitar conflictos con 'Model'
    - Compatible con la arquitectura existente de repositorios y singleton de base de datos

--- Fuerza_G_Taller/Pages/Vehicle/Edit.cshtml.cs
+++ Fuerza_G_Taller/Pages/Vehicle/Edit.cshtml.cs
@@ -1,12 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VehicleEntity = Fuerza_G_Taller.Models.Vehicle;
+using Fuerza_G_Taller.Repositories;
 
-namespace Fuerza_G_Taller.Pages.Model
+namespace Fuerza_G_Taller.Pages.Vehicle
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly VehicleRepository _vehicleRepository;
+        private readonly OwnerRepository _ownerRepository;
+        private readonly ModelRepository _modelRepository;
+
+        public EditModel(VehicleRepository vehicleRepository,
+                         OwnerRepository ownerRepository,
+                         ModelRepository modelRepository)
         {
+            _vehicleRepository = vehicleRepository;
+            _ownerRepository = ownerRepository;
+            _modelRepository = modelRepository;
+        }
+
+        [BindProperty]
+        public VehicleEntity Vehicle { get; set; } = new VehicleEntity();
+
+        public IEnumerable<SelectListItem> OwnersSelectList { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> ModelsSelectList { get; set; } = new List<SelectListItem>();
+
+        public IActionResult OnGet(int id)
+        {
+            var vehicleEntity = _vehicleRepository.GetById(id);
+            if (vehicleEntity == null) return RedirectToPage("Index");
+
+            Vehicle = vehicleEntity;
+
+            OwnersSelectList = _ownerRepository.GetAll()
+                .Select(o => new SelectListItem { Value = o.Id.ToString(), Text = o.Name + " " + o.FirstLastName });
+
+            ModelsSelectList = _modelRepository.GetAll()
+                .Select(m => new SelectListItem { Value = m.Id.ToString(), Text = m.Name });
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            _vehicleRepository.Update(Vehicle);
+            return RedirectToPage("Index");
         }
     }
 }
